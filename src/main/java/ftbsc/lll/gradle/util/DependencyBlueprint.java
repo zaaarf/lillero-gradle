82ff23dd0dc85d407e8ec42c3b543c7eaf61dae0
zaaarf <me@zaaarf.foo>
2026-07-21 12:20:24 +0200

    docs: make javadoc task happy

--- src/main/java/ftbsc/lll/gradle/util/DependencyBlueprint.java
+++ src/main/java/ftbsc/lll/gradle/util/DependencyBlueprint.java
@@ -7,9 +7,6 @@ import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.provider.Property;
 import org.gradle.api.provider.Provider;
 
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
-
 /**
  * Wrapper around Provider that can be recreated on demand.
  * Needed because each Provider must be used once, apparently.
@@ -22,6 +19,10 @@ public class DependencyBlueprint {
 	private final Property<String> version;
 	private ApplyCondition condition = null;
 
+	/**
+	 * Create the dependency provider from the blueprint.
+	 * @return the provider
+	 */
 	public Provider<Dependency> build() {
 		if(this.condition == null) {
 			return dependency(this.project, this.depstring, this.version);
@@ -36,12 +37,29 @@ public class DependencyBlueprint {
 		}
 	}
 
+	/**
+	 * Create a dependency provider from the given parameters
+	 * @param project the project
+	 * @param depstring the depstring
+	 * @param version the version provider
+	 * @return the provider
+	 */
 	public static Provider<Dependency> dependency(Project project, String depstring, Property<String> version) {
 		return version.orElse("+").map(v -> project.getDependencies().create(depstring + v));
 	}
 
+	/**
+	 * A condition for creating a dependency.
+	 */
 	@FunctionalInterface
 	public interface ApplyCondition {
+		/**
+		 * The actual condition.
+		 * @param project the project
+		 * @param depstring the depstring
+		 * @param versionProvider the version provider
+		 * @return whether it should be applied
+		 */
 		boolean should(Project project, String depstring, Provider<String> versionProvider);
 	}
 }
